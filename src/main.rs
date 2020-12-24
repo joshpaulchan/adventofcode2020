48ccc73e0eb76d8cf0ba060a0a65b46a03878765
Joshua Paul Chan <joshpaulchan@gmail.com>
2020-12-23 23:55:03 -0500

    basic input test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = parse_config(&args);
+
+    print!("Running: {} ", config.problem);
+    println!("with input: {}", config.input);
+
+    let contents = fs::read_to_string(config.input).expect("Something went wrong reading the file");
+
+    println!("{}", contents);
+}
+
+struct Parameters {
+    problem: String,
+    input: String,
+}
+
+fn parse_config(args: &[String]) -> Parameters {
+    let problem = args[1].clone();
+    let input = args[2].clone();
+
+    Parameters { problem, input }
 }
