621bc59ee3af97eee0a61355b7ed22bb10bfd220
Joshua Paul Chan <joshpaulchan@gmail.com>
2020-12-24 21:27:01 -0500

    use implicit return for config parser

--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,8 @@ struct Parameters {
 }
 
 fn parse_config(args: &[String]) -> Option<Parameters> {
-    return Some(Parameters {
+    Some(Parameters {
         problem: args[1].clone(),
         args: args[2..].to_vec(),
-    });
+    })
 }
