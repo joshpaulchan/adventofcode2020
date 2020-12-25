999a37acc46782bf0ab22bc49166a78516d07d0b
Joshua Paul Chan <joshpaulchan@gmail.com>
2020-12-24 22:45:23 -0500

    add day-by-day handling

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = adventofcode::parse_config(&args).expect("Parameters should be given.");
-
     print!("Running: {} with args: {:?}", config.problem, config.args);
 
-    adventofcode::dump_file(config);
+    match config.problem.as_ref() {
+        "1" => println!("Day 1."),
+        _ => adventofcode::dump_file(config),
+    }
 }
