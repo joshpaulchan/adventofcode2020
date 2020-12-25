5280240002cab4911837db6af057790e4a4bde74
Joshua Paul Chan <joshpaulchan@gmail.com>
2020-12-24 21:42:47 -0500

    rename file to make sure rust analyzer works

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use std::env;
 use std::fs;
 
-use adventofcode2020::Parameters;
-
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = adventofcode2020::parse_config(&args).expect("Parameters should be given.");
+    let config = adventofcode::parse_config(&args).expect("Parameters should be given.");
 
     print!("Running: {} with args: {:?}", config.problem, config.args);
 
