73332dfbfdff424706db969ba89964f5c498f17b
Joshua Paul Chan <joshpaulchan@gmail.com>
2020-12-24 23:35:24 -0500

    inline numbers

--- src/day1.rs
+++ src/day1.rs
@@ -2,20 +2,20 @@ use adventofcode::Parameters;
 use std::fs;
 
 pub fn run(params: Parameters) {
-    let contents = fs::read_to_string(
+    let numbers: Vec<i32> = fs::read_to_string(
         params
             .args
             .first()
             .map(|filename| filename.to_string())
-            .expect("There was no filename given."),
+            .expect("Something went wrong reading the file"),
     )
-    .expect("Something went wrong reading the file");
-    // println!("{}", contents);
+    .expect("Something went wrong reading the file")
+    .split_ascii_whitespace()
+    .map(|line| line.trim().parse().expect("Didn't get a number."))
+    .collect();
 
-    let numbers: Vec<i32> = contents
-        .split_ascii_whitespace()
-        .map(|line| line.trim().parse().expect("Didn't get a number."))
-        .collect();
+    // TODO: take in goal(s)
+    // TODO: solve
 
     let sum: i32 = numbers.iter().sum();
 
