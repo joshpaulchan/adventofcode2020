bed96913f8b2756d9fb4ca4ea70cfb2b1824a81f
Joshua Paul Chan <joshpaulchan@gmail.com>
2020-12-24 22:09:31 -0500

    move file dumper to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 pub struct Parameters {
     pub problem: String,
     pub args: Vec<String>,
@@ -8,4 +10,17 @@ pub fn parse_config(args: &[String]) -> Option<Parameters> {
         problem: args[1].clone(),
         args: args[2..].to_vec(),
     })
+}
+
+pub fn dump_file(params: Parameters) {
+    let contents = fs::read_to_string(
+        params
+            .args
+            .first()
+            .map(|filename| filename.to_string())
+            .expect("There was no filename given."),
+    )
+    .expect("Something went wrong reading the file");
+
+    println!("{}", contents);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::env;
-use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,14 +7,5 @@ fn main() {
 
     print!("Running: {} with args: {:?}", config.problem, config.args);
 
-    let contents = fs::read_to_string(
-        config
-            .args
-            .first()
-            .map(|filename| filename.to_string())
-            .expect("There was no filename given."),
-    )
-    .expect("Something went wrong reading the file");
-
-    println!("{}", contents);
+    adventofcode::dump_file(config);
 }
