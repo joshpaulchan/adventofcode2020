445b617830fd6c09e8f1e3fc4c1e979c5530db97
Joshua Paul Chan <joshpaulchan@gmail.com>
2020-12-24 23:14:07 -0500

    print their sum

--- src/day1.rs
+++ src/day1.rs
@@ -1,5 +1,6 @@
 use adventofcode::Parameters;
 use std::fs;
+
 pub fn run(params: Parameters) {
     let contents = fs::read_to_string(
         params
@@ -9,5 +10,14 @@ pub fn run(params: Parameters) {
             .expect("There was no filename given."),
     )
     .expect("Something went wrong reading the file");
-    println!("{}", contents);
+    // println!("{}", contents);
+
+    let numbers: Vec<i32> = contents
+        .split_ascii_whitespace()
+        .map(|line| line.trim().parse().expect("Didn't get a number."))
+        .collect();
+
+    let sum: i32 = numbers.iter().sum();
+
+    println!("their sum: {}", sum);
 }
